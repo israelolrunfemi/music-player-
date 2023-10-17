e0d782ad0db88420e7437385096f2858dea5e47d
Israel  Olorunfemi <107123944+israelolrunfemi@users.noreply.github.com>
2023-10-17 21:02:11 +0100

    Add files via upload

---
+++ Music-List.js
@@ -0,0 +1,46 @@
+
+let allMusic = [
+  {
+    name: "Alex action",
+    artist: "John Israel",
+    img: "music-1",
+    src: "Alex action",
+  },
+  {
+    name: "A Long Way",
+    artist: "Dame Sad",
+    img: "music-2",
+    src: "a-long-way",
+  },
+  {
+    name: "Daylight",
+    artist: "Beauz & Mark",
+    img: "music-3",
+    src: "daylight",
+  },
+  {
+    name: "Forest-Lullaby",
+    artist: "Lesfm",
+    img: "music-4",
+    src: "forest-lullaby"
+  },
+  {
+    name: "Lost In The City",
+    artist: "Cosmo Sheldrake",
+    img: "music-5",
+    src: "lost-in-city-lights",
+  },
+  {
+    name: "Lost Sky",
+    artist: "Apple x Orange",
+    img: "music-6",
+    src: "powerful-beat",
+  },
+  {
+      name: "Risk",
+      artist: "lorem",
+      img: "music-1",
+      src: "Risk",
+    },
+  
+];
